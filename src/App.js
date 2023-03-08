083fb76861354e740f9e72e2287a5f50d79125dc
useonguk <ysu0219@gmail.com>
2023-03-08 17:58:06 +0900

    toDolist 기초

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+import { useState } from 'react';
+
+function App(){
+  const [toDo,setTodo] = useState("");
+  const [toDos,setTodos] = useState([]);
+  // toDo 라는 state를 관리하는 setTodo 함수.
+  const onChange = (e) =>{
+    setTodo(e.target.value)
+  }
+
+  const onSubmit = (e) =>{
+    e.preventDefault();
+    if(toDo === ""){
+      return
+    }
+    setTodos((currentArray) => [toDo, ...currentArray])
+    setTodo("")
+  }
+  console.log(toDos);
+	return (
+    <div>
+        <h1>투두리스트를 만들어보자~</h1>
+        <form onSubmit={onSubmit}>
+            <input type="text" placeholder="입력하세요" onChange={onChange} value={toDo} />
+            <button>등록하기</button>
+        </form>
+        <ul>
+          {toDos.map((item,index)=><li key={index}>{item}</li>)}
+        </ul>
+    </div>
+    )
+}
+
+export default App;
